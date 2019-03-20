4acb5f09a36b96a15c8b7780e1f400becef755a0
Laura Badgley <l.badgleyy@gmail.com>
2019-03-20 12:04:37 -0700

    refactored favorite-component test

---
+++ src/favorite-component.js
@@ -0,0 +1,30 @@
+export function createSchemeLi(favorite) {
+    const html = /*html*/ `
+        <li class="scheme-display">
+            <div><span class="favorite-heart">♡</span></div>
+            <section class="color-display">
+                <p>#${favorite.scheme[0]}</p>
+                <div style="background-color: #${favorite.scheme[0]};"></div>
+            </section>
+            <section class="color-display">
+                <p>#${favorite.scheme[1]}</p>
+                <div style="background-color: #${favorite.scheme[1]};"></div>
+            </section>
+            <section class="color-display">
+                <p>#${favorite.scheme[2]}</p>
+                <div style="background-color: #${favorite.scheme[2]};"></div>
+            </section>
+            <section class="color-display">
+                <p>#${favorite.scheme[3]}</p>
+                <div style="background-color: #${favorite.scheme[3]};"></div>
+            </section>
+            <section class="color-display">
+                <p>#${favorite.scheme[4]}</p>
+                <div style="background-color: #${favorite.scheme[4]};"></div>
+            </section>
+        </li>
+    `;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
--- tests/favorite-component.test.js
+++ tests/favorite-component.test.js
@@ -1,36 +1,6 @@
+import { createSchemeLi } from '../src/favorite-component.js';
 const test = QUnit.test;
 QUnit.module('favorites page test');
-
-function createSchemeLi(favorite) {
-    const html = /*html*/ `
-        <li class="scheme-display">
-            <div><span class="favorite-heart">♡</span></div>
-            <section class="color-display">
-                <p>#${favorite.scheme[0]}</p>
-                <div style="background-color: #${favorite.scheme[0]};"></div>
-            </section>
-            <section class="color-display">
-                <p>#${favorite.scheme[1]}</p>
-                <div style="background-color: #${favorite.scheme[1]};"></div>
-            </section>
-            <section class="color-display">
-                <p>#${favorite.scheme[2]}</p>
-                <div style="background-color: #${favorite.scheme[2]};"></div>
-            </section>
-            <section class="color-display">
-                <p>#${favorite.scheme[3]}</p>
-                <div style="background-color: #${favorite.scheme[3]};"></div>
-            </section>
-            <section class="color-display">
-                <p>#${favorite.scheme[4]}</p>
-                <div style="background-color: #${favorite.scheme[4]};"></div>
-            </section>
-        </li>
-    `;
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content;
-}
 
 test('create dom for faves', assert => {
     // arrange
