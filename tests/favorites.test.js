9dae51cd99c1d7fba25b62c22258c317883f88a1
Laura Badgley <l.badgleyy@gmail.com>
2019-03-20 12:02:33 -0700

    interpollated favorites template

--- tests/favorites.test.js
+++ tests/favorites.test.js
@@ -1,32 +1,35 @@
 const test = QUnit.test;
 QUnit.module('favorites page test');
 
-function createSchemeLi() {
-    return /*html*/ `
+function createSchemeLi(favorite) {
+    const html = /*html*/ `
         <li class="scheme-display">
             <div><span class="favorite-heart">♡</span></div>
             <section class="color-display">
-                <p>#74fdbf</p>
-                <div style="background-color: #74fdbf;"></div>
+                <p>#${favorite.scheme[0]}</p>
+                <div style="background-color: #${favorite.scheme[0]};"></div>
             </section>
             <section class="color-display">
-                <p>#6BF9B9</p>
-                <div style="background-color: #6BF9B9;"></div>
+                <p>#${favorite.scheme[1]}</p>
+                <div style="background-color: #${favorite.scheme[1]};"></div>
             </section>
             <section class="color-display">
-                <p>#6FFAE5</p>
-                <div style="background-color: #6FFAE5;"></div>
+                <p>#${favorite.scheme[2]}</p>
+                <div style="background-color: #${favorite.scheme[2]};"></div>
             </section>
             <section class="color-display">
-                <p>#73E7FB</p>
-                <div style="background-color: #73E7FB;"></div>
+                <p>#${favorite.scheme[3]}</p>
+                <div style="background-color: #${favorite.scheme[3]};"></div>
             </section>
             <section class="color-display">
-                <p>#78C1FC</p>
-                <div style="background-color: #78C1FC;"></div>
+                <p>#${favorite.scheme[4]}</p>
+                <div style="background-color: #${favorite.scheme[4]};"></div>
             </section>
         </li>
     `;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
 }
 
 test('create dom for faves', assert => {
@@ -56,12 +59,20 @@ test('create dom for faves', assert => {
             </section>
         </li>
     `;
-    
+    const favorite = {
+        scheme: {
+            0: '74fdbf',
+            1: '6BF9B9',
+            2: '6FFAE5',
+            3: '73E7FB',
+            4: '78C1FC',
+        }
+    };
     // act
-    const result = createSchemeLi();
+    const result = createSchemeLi(favorite);
 
 
     // assert
-    assert.equal(result, expected);
+    assert.htmlEqual(result, expected);
     
 });
