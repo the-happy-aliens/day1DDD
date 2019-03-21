cd2e5e320e681dd607cc1a78f8d7fec7391e1d6c
Laura Badgley <l.badgleyy@gmail.com>
2019-03-20 17:37:58 -0700

    microcommit

--- src/coloring-page.js
+++ src/coloring-page.js
@@ -1,4 +1,4 @@
-import littleGuyFillSvg from './coloring/little-guy.js';
+// import littleGuyFillSvg from './coloring/little-guy.js';
 import loadHeader from './header-component.js';
 import littleDude from '../assets/little-dude.js';
 import { auth, usersFavoriteColorSchemesRef } from './firebase.js';
